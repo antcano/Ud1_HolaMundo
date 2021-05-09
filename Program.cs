e8b3075bab6d767741771be89e32be0f72b76e0a
antcano <83769758+antcano@users.noreply.github.com>
2021-05-09 20:03:26 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ud1_holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecucion hasta presionar tecla
+        }
+    }
+}
